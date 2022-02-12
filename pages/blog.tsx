3a39355cb31f3820a95ac130fe01f363337b5862
mr-srivastava <aadarsh.srivastava.007@gmail.com>
2022-02-12 19:04:33 +0530

    Add Work in progress for pages coming soon

---
+++ pages/blog.tsx
@@ -0,0 +1,13 @@
+import WIP from '@Components/WIP'
+import HomeLayout from '@Layouts/HomeLayout'
+import React from 'react'
+
+function Blog() {
+  return (
+    <HomeLayout>
+      <WIP />
+    </HomeLayout>
+  )
+}
+
+export default Blog
