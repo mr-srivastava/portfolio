e9aec70e83412a8eec6840ae4da633f21cfba66f
mr-srivastava <aadarsh.srivastava.007@gmail.com>
2022-02-21 23:31:16 +0530

    download icon

--- components/ResumeViewer/index.tsx
+++ components/ResumeViewer/index.tsx
@@ -32,10 +32,24 @@ const ResumeViewer = () => {
           <div className="flex w-full items-center justify-between bg-gray-800 px-3 py-3 text-white">
             <p className="text-2xl">Resume</p>
             <a
-              className="rounded bg-blue-600 px-2 py-2 hover:bg-blue-500"
+              className="flex rounded bg-blue-600 px-2 py-2 hover:bg-blue-500"
               href="./resume.pdf"
               download
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                />
+              </svg>
               Download
             </a>
           </div>
