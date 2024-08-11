6173185ab9d87bf931c27d01999a302559ea57a2
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2024-08-11 16:16:27 +0530

    reset the protfolio website

--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/resume', title: 'Resume' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-];
-
-export default headerNavLinks;
