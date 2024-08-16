2dc4cdc0015e7777d3049c9e189d7161e0c46d70
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2024-08-16 11:14:17 +0530

    skills section

--- src/components/blocks/hover-grid.tsx
+++ src/components/blocks/hover-grid.tsx
@@ -1,68 +1,79 @@
 import { cn } from "@/lib/utils";
 import {
   IconAdjustmentsBolt,
-  IconCloud,
-  IconCurrencyDollar,
-  IconEaseInOut,
+  IconBrandDocker,
+  IconBrandGit,
+  IconBrandGoogleAnalytics,
+  IconBrandJavascript,
+  IconBrandNextjs,
+  IconBrandNodejs,
+  IconBrandReact,
+  IconBrandRedux,
+  IconBrandTypescript,
   IconHeart,
   IconHelp,
-  IconRouteAltLeft,
-  IconTerminal2,
+  IconRouteAltLeft
 } from "@tabler/icons-react";
 
 export default function HoverGrid() {
   const features = [
     {
-      title: "Built for developers",
+      title: "React",
       description:
         "Built for engineers, developers, dreamers, thinkers and doers.",
-      icon: <IconTerminal2 />,
+      icon: <IconBrandReact />
     },
     {
-      title: "Ease of use",
+      title: "Next",
       description:
         "It's as easy as using an Apple, and as expensive as buying one.",
-      icon: <IconEaseInOut />,
+      icon: <IconBrandNextjs />
     },
     {
-      title: "Pricing like no other",
+      title: "Javascript",
       description:
         "Our prices are best in the market. No cap, no lock, no credit card required.",
-      icon: <IconCurrencyDollar />,
+      icon: <IconBrandJavascript />
     },
     {
-      title: "100% Uptime guarantee",
+      title: "Typescript",
       description: "We just cannot be taken down by anyone.",
-      icon: <IconCloud />,
+      icon: <IconBrandTypescript />
     },
     {
-      title: "Multi-tenant Architecture",
-      description: "You can simply share passwords instead of buying new seats",
-      icon: <IconRouteAltLeft />,
-    },
-    {
-      title: "24/7 Customer Support",
+      title: "Redux",
       description:
         "We are available a 100% of the time. Atleast our AI Agents are.",
-      icon: <IconHelp />,
+      icon: <IconBrandRedux />
     },
     {
-      title: "Money back guarantee",
+      title: "Node",
+      description: "You can simply share passwords instead of buying new seats",
+      icon: <IconBrandNodejs />
+    },
+
+    {
+      title: "Git",
       description:
         "If you donot like EveryAI, we will convince you to like us.",
-      icon: <IconAdjustmentsBolt />,
+      icon: <IconBrandGit />
     },
     {
-      title: "And everything else",
+      title: "Docker",
       description: "I just ran out of copy ideas. Accept my sincere apologies",
-      icon: <IconHeart />,
-    },
+      icon: <IconBrandDocker />
+    }
   ];
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  relative z-10 py-10 max-w-7xl mx-auto">
-      {features.map((feature, index) => (
-        <Feature key={feature.title} {...feature} index={index} />
-      ))}
+    <div className='py-20 lg:py-40 h-screen flex flex-col items-center justify-center'>
+      <h2 className='md:text-4xl text-xl text-white relative z-20'>
+        Skill-set
+      </h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  relative z-10 py-10 max-w-7xl mx-auto'>
+        {features.map((feature, index) => (
+          <Feature key={feature.title} {...feature} index={index} />
+        ))}
+      </div>
     </div>
   );
 }
@@ -71,7 +82,7 @@ const Feature = ({
   title,
   description,
   icon,
-  index,
+  index
 }: {
   title: string;
   description: string;
@@ -87,21 +98,21 @@ const Feature = ({
       )}
     >
       {index < 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+        <div className='opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none' />
       )}
       {index >= 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+        <div className='opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none' />
       )}
-      <div className="mb-4 relative z-10 px-10 text-neutral-600 dark:text-neutral-400">
+      <div className='mb-4 relative z-10 px-10 text-neutral-600 dark:text-neutral-400'>
         {icon}
       </div>
-      <div className="text-lg font-bold mb-2 relative z-10 px-10">
-        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
-        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
+      <div className='text-lg font-bold mb-2 relative z-10 px-10'>
+        <div className='absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center' />
+        <span className='group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100'>
           {title}
         </span>
       </div>
-      <p className="text-sm text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10">
+      <p className='text-sm text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10'>
         {description}
       </p>
     </div>
