387b9ad8473e0b39a12984f10b9e62fcf3c15a4f
Aadarsh Srivastava <aadarsh.s@zolostays.com>
2024-08-17 18:18:47 +0530

    added work ex

--- src/components/WorkEx/WorkEx.tsx
+++ src/components/WorkEx/WorkEx.tsx
@@ -5,7 +5,7 @@ export default function WorkExperienceSection() {
   return (
     <div className='p-20 min-h-screen flex flex-col items-center'>
       <h2
-        className='font-display text-center drop-shadow-sm text-white w-fit tracking-[-2px] text-6xl font-bold mb-10'
+        className='font-display text-center drop-shadow-sm text-white w-fit tracking-[-2px] text-6xl font-bold mb-20'
         style={{ filter: "blur(0px)", opacity: 1, transform: "none" }}
       >
         Work Experience
