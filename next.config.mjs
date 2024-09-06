1bf14a5e01676bc07e4a20b2df5b1118b588b61e
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2024-09-06 10:13:02 +0530

    update next config for github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/portfolio",
+  output: "export",
+  reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com", "assets.aceternity.com"],
   },
