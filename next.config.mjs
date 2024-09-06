1c2658cec58bf65956ff2c2139b6cae7e9f115b6
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2024-09-06 18:11:05 +0530

    imageKit SDK setup

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["ik.imagekit.io", "assets.aceternity.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+      },
+    ],
   },
   experimental: {
     optimizePackageImports: ["@tabler/icons-react"],
