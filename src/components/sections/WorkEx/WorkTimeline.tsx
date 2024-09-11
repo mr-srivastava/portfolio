dbc6ea7d941daa4828be538a5ad93edd6019e2c3
Aadarsh Srivastava <58096256+mr-srivastava@users.noreply.github.com>
2024-09-11 22:18:34 +0530

    Develop (#37)
    * add company logos
    
    * move the experience data to utils
    
    ---------
    
    Co-authored-by: Aadarsh Srivastava <aadarsh.s@zolostays.com>

--- src/components/sections/WorkEx/WorkTimeline.tsx
+++ src/components/sections/WorkEx/WorkTimeline.tsx
@@ -1,7 +1,7 @@
 import { Timeline } from "@/components/ui/timeline";
 import React from "react";
-import { getExperienceTimelineData } from "./helper";
 import Experience from "./Experience";
+import { getExperienceTimelineData } from "@/utils";
 
 const EXPERIENCE_TIMELINE_DATA = getExperienceTimelineData().map(
   (companyHistory) => {
@@ -9,19 +9,20 @@ const EXPERIENCE_TIMELINE_DATA = getExperienceTimelineData().map(
       title: companyHistory.company,
       content: (
         <Experience
+          id={companyHistory.id}
           positions={companyHistory.positions}
           startDate={companyHistory.companyStartDate}
           endDate={companyHistory.companyEndDate}
           company={companyHistory.company}
         />
-      )
+      ),
     };
   }
 );
 
 export default function WorkTimeline() {
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <Timeline data={EXPERIENCE_TIMELINE_DATA} />
     </div>
   );
