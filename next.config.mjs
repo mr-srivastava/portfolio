23bc00d58c810a0db3b94e91867b972e4462a297
Aadarsh Srivastava <58096256+mr-srivastava@users.noreply.github.com>
2024-09-18 22:34:11 +0530

    Develop (#43)
    * add company logos
    
    * move the experience data to utils
    
    * Mobile site (#38)
    
    * Develop (#37)
    
    * add company logos
    
    * move the experience data to utils
    
    ---------
    
    Co-authored-by: Aadarsh Srivastava <aadarsh.s@zolostays.com>
    
    * deploy to domain
    
    * deploy to domain
    
    * responsive hero section
    
    * responsive overview section
    
    * responsive experience section
    
    * uncomment unoptimized component
    
    ---------
    
    Co-authored-by: Aadarsh Srivastava <aadarsh.s@zolostays.com>
    
    * Mobile site (#40)
    
    * responsive hero section
    
    * responsive overview section
    
    * responsive experience section
    
    * uncomment unoptimized component
    
    * color correction
    
    ---------
    
    Co-authored-by: Aadarsh Srivastava <aadarsh.s@zolostays.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    // unoptimized: true,
     domains: ["ik.imagekit.io", "assets.aceternity.com"],
     remotePatterns: [
       {
